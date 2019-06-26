82188b7f0d816e604af5778ae74579a208152e63
MikhailRommulo <mikhailrommulo@gmail.com>
2019-06-26 14:24:55 -0300

    Fechar a janela com o botão cancelar em novo produto

--- src/view/ProdutoNovo.java
+++ src/view/ProdutoNovo.java
@@ -183,6 +183,11 @@ public class ProdutoNovo extends JDialog {
 			}
 			{
 				JButton cancelButton = new JButton("Cancelar");
+				cancelButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent arg0) {
+						dispose();
+					}
+				});
 				cancelButton.setActionCommand("Cancel");
 				buttonPane.add(cancelButton);
 			}
